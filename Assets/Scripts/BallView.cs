7938c537a93233e6d91f4245ad75e8a3f8ee4e57
Vladimir Skoblo <rregacc@gmail.com>
2019-12-08 22:28:36 +0300

    init commit

---
+++ Assets/Scripts/BallView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallView : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer renderer;
+
+    private void Start()
+    {
+        if (renderer == null)
+            renderer = GetComponent<MeshRenderer>();
+    }
+
+    internal void SetColor(Color color)
+    {
+        if (renderer != null)
+        {
+            renderer.sharedMaterial.color = color;
+        }
+    }
+}
