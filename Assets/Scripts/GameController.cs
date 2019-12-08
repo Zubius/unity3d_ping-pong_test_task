1fefa77a1c9939c789327222d2bf26c607362b07
Vladimir Skoblo <rregacc@gmail.com>
2019-12-09 01:30:38 +0300

    base done

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
@@ -8,7 +10,12 @@ public class GameController : MonoBehaviour
     [SerializeField] private IRacketInput playerRacket;
     [SerializeField] private IRacketInput rivalRacket;
 
+    [SerializeField] private Text countText;
+    [SerializeField] private Text myScoreText;
+    [SerializeField] private Text rivalScoreText;
+
     private int _myScore, _rivalScore;
+    private WaitForSeconds _waitFor1sec = new WaitForSeconds(1);
 
     private void Start()
     {
@@ -17,6 +24,7 @@ public class GameController : MonoBehaviour
             ball.OnScored += OnScored;
         }
 
+        ball.transform.localPosition = new Vector2(10000, 10000);
         Restart();
     }
 
@@ -25,26 +33,44 @@ public class GameController : MonoBehaviour
         _myScore = 0;
         _rivalScore = 0;
 
-        LaunchBall();
+        myScoreText.text = _myScore.ToString();
+        rivalScoreText.text = _rivalScore.ToString();
+
+        StartCoroutine(LaunchBall());
     }
 
-    private void LaunchBall()
+    private IEnumerator LaunchBall()
     {
+        countText.text = "3";
+        yield return _waitFor1sec;
+        countText.text = "2";
+        yield return _waitFor1sec;
+        countText.text = "1";
+        yield return _waitFor1sec;
+        countText.text = string.Empty;
+
         ball.transform.localPosition = Vector3.zero;
         ball.SetSize(Random.Range(0.1f, 1f));
         ball.SetColor(Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
-        ball.Launch(new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)), Random.Range(1f, 10f));
+        ball.Launch(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)), Random.Range(1f, 10f));
     }
 
     private void OnScored(Collision obj)
     {
         if (obj.transform.localPosition.y > 0)
+        {
             _myScore++;
+            myScoreText.text = _myScore.ToString();
+        }
 
         if (obj.transform.localPosition.y < 0)
+        {
             _rivalScore++;
+            rivalScoreText.text = _rivalScore.ToString();
+        }
 
         ball.transform.localPosition = new Vector2(10000, 10000);
+        StartCoroutine(LaunchBall());
     }
 
     private void OnDestroy()
